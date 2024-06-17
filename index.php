def782388f1ab5b5e022b75408761bbc480a1802
Fighting123567 <150749316+Fighting123567@users.noreply.github.com>
2024-06-17 13:52:21 +0700

    index.php

--- index.php
+++ index.php
@@ -1,2 +1,39 @@
-<?php
-echo "hello Github";
+<?php 
+ob_start();//Ojcet start
+session_start();
+// include_once('Model/hang-hoa.php');
+include_once('Model//dao/loai.php');
+//điều hướng đến các contronller
+$allloai=loai_select_all();
+if(isset($_GET['mod'])){
+  switch ($_GET['mod']){
+      case 'page':
+          include_once 'Controller/HomeController.php';
+          break;
+      case 'pro':
+          include_once 'Controller/ProductController.php';
+          break;
+      case 'cat':
+          include_once 'Controller/CategoryController.php';
+          break;
+      case 'user':
+          include_once 'Controller/UserController.php';
+          break;
+          case 'cart':
+            include_once 'Controller/cart.php';
+            break;
+          case 'order':
+            include_once 'Controller/order.php';
+            break;
+      case 'admin':
+        include_once 'Controller/AdminController.php';
+        break;
+      default:
+      #code
+
+  }
+}
+else{
+  header("Location: ?mod=page&act=home");
+}
+?>
